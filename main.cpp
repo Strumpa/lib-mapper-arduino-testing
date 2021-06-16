c9c827b510b3f0d970e85c45928f8ae7f90d7ef3
Strumpa <77298901+Strumpa@users.noreply.github.com>
2021-06-16 16:16:20 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ using namespace mapper;
 // For disabling power saving
 #include "esp_wifi.h"
 
-const char* ssid     = "WIFI_SSID";
-const char* password = "WIFI_PASSWORD";
+#define SIG_SIZE 7
+
+const char* ssid     = "MONA_LISA";
+const char* password = "octogone93";
 
 const int NUM_SIGNALS = 1;
-const int SIG_SIZE = 6;
+
 
 Device* dev;
 std::vector<Signal> signals;
@@ -43,23 +45,26 @@ void setup() {
 
   dev = new Device("ESP32");
   for (int i = 0; i < NUM_SIGNALS; ++i) {
+    
     std::ostringstream sigName;
     sigName << "test" << i;
     float min = 0.0f;
-    float max = 5.0f;
-    signals.push_back(dev->add_signal(Direction::OUTGOING, sigName.str(), SIG_SIZE,
-                                      Type::FLOAT, "V", &min, &max));
+    float max = 10.0f;
+    Serial.println("creating sig");
+    signals.push_back(dev->add_signal(Direction::OUTGOING, sigName.str(), SIG_SIZE, Type::FLOAT, "V", &min, &max));
+    Serial.println("adding sig");
   }
 }
 
 void loop() {
   dev->poll();
   for (int i = 0; i < NUM_SIGNALS; ++i) {
+    //Serial.println("creating vector");
     std::vector <float> vec (SIG_SIZE);
     for (int j = 0; j < SIG_SIZE; ++j){
-      vec.push_back(j);
+      vec[j] = 1;
     }
     signals[i].set_value(vec);
-    #Serial.println("adding vec to signal");
+    //Serial.println("adding vec to signal");
   }
 }
